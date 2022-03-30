7530665a5a4a5b06cd0d039bb6a79a5712585182
bigth8 <nglow90@gmail.com>
2022-03-30 17:17:58 +0900

    Complete usage of rest template in msa

--- e-commerce/user-service/src/main/java/com/example/msa/ecommerce/userservice/UserServiceApplication.java
+++ e-commerce/user-service/src/main/java/com/example/msa/ecommerce/userservice/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.msa.ecommerce.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +17,7 @@ public class UserServiceApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
